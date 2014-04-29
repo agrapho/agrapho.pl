dade0f7667c1e820d482bbdf98be1a034dd8b8c1
Kuba Odias <agrapho.pl@gmail.com>
2014-04-29 20:49:50 +0200

    jodias: Colorbox gallery.

--- sliding-gallery.php
+++ sliding-gallery.php
@@ -14,9 +14,31 @@ Template Name: Sliding Gallery Page
                           'post_parent' => $post->ID,
                           'posts_per_page' => -1 );
             $images = get_posts( $args );
+            $colorbox_gallery_category = get_category_by_slug('colorbox-gallery');
             foreach($images as $image):
-                echo '<li><a href="#"><img src="' . wp_get_attachment_image_src($image->ID, 'thumbnail')[0] . '"></img></a></li>';
-            endforeach;
+                $subgallery_name = $image->post_excerpt;
+                $args = array('name' => $subgallery_name,
+                              'category' => $colorbox_gallery_category->cat_ID);
+                $subgallery_posts = get_posts( $args );
+                foreach($subgallery_posts as $subgallery):
+                    $args = array('post_type' => 'attachment',
+                                  'post_mime_type' => 'image',
+                                  'post_parent' => $subgallery->ID,
+                                  'posts_per_page' => -1 );
+                    $image_subgallery = get_posts( $args ); ?>
+                    <li>
+                    <?php $is_first_sub_image = true;
+                          foreach($image_subgallery as $sub_image):
+                              echo '<a href="' . wp_get_attachment_image_src($sub_image->ID, 'full')[0] . '" class="colorbox colorbox-' . $subgallery_name . '" rel="' . $subgallery_name . '" title="' . $sub_image->post_excerpt . '">';
+                              if ($is_first_sub_image) {
+                                  echo '<img src="' . wp_get_attachment_image_src($image->ID, 'thumbnail')[0] . '"></img>';
+                              }
+                              $is_first_sub_image = false;
+                              echo '</a>';
+                            endforeach; ?>
+                    </li>
+      <?php     endforeach; // END $subgallery_posts
+            endforeach; // END $images
       ?>
       </ul>
       <!-- End Elastislide Carousel -->
@@ -28,4 +50,5 @@ Template Name: Sliding Gallery Page
 <script type="text/javascript" src="<?php echo get_template_directory_uri() ?>/elastislide/js/jquery.elastislide.js"></script>
 <script type="text/javascript">
     jQuery( '#carousel' ).elastislide( { minItems : 2 } );
+    jQuery( 'a.colorbox' ).colorbox({maxWidth:'95%', maxHeight:'95%'});
 </script>
