c0ab54a18be6992b7fc4b58bc8a2bc5cc2df12bc
Kuba Odias <agrapho.pl@gmail.com>
2014-05-14 21:18:03 +0200

    Sliding gallery fixes.

--- sliding-gallery.php
+++ sliding-gallery.php
@@ -14,25 +14,34 @@ Template Name: Sliding Gallery Page
             $colorbox_gallery_category = get_category_by_slug('colorbox-gallery');
             foreach($images as $image):
                 $subgallery_name = $image->post_excerpt;
-                $args = array('name' => $subgallery_name,
-                              'category' => $colorbox_gallery_category->cat_ID);
-                $subgallery_posts = get_posts( $args );
-                foreach($subgallery_posts as $subgallery):
-                    $image_subgallery = get_attachments_by_parent_id( $subgallery->ID ); ?>
-                    <li>
-                    <?php $is_first_sub_image = true;
-                          foreach($image_subgallery as $sub_image):
-                              $subgallery_attachments = wp_get_attachment_image_src( $sub_image->ID, 'full');
-                              echo '<a href="' . $subgallery_attachments[0] . '" class="colorbox colorbox-' . $subgallery_name . '" rel="' . $subgallery_name . '" title="' . $sub_image->post_excerpt . '">';
-                              if ($is_first_sub_image) {
-                                  $gallery_attachments = wp_get_attachment_image_src( $image->ID, 'thumbnail');
-                                  echo '<img src="' . $gallery_attachments[0] . '"></img>';
-                              }
-                              $is_first_sub_image = false;
-                              echo '</a>';
+                if ($subgallery_name == "") {
+                    $subgallery_name = $image->post_name;
+                    $subgallery_attachments = wp_get_attachment_image_src( $image->ID, 'full');
+                    echo '<li><a href="' . $subgallery_attachments[0] . '" class="colorbox colorbox-' . $subgallery_name . '" rel="' . $subgallery_name . '" title="' . $image->post_excerpt . '">';
+                    $subgallery_attachments = wp_get_attachment_image_src( $image->ID, 'thumbnail');
+                    echo '<img src="' . $subgallery_attachments[0] . '"></img>';
+                    echo '</a></li>';
+                } else {
+                    $args = array('name' => $subgallery_name,
+                                  'category' => $colorbox_gallery_category->cat_ID);
+                    $subgallery_posts = get_posts( $args );
+                    foreach($subgallery_posts as $subgallery):
+                        $image_subgallery = get_attachments_by_parent_id( $subgallery->ID ); ?>
+                        <li>
+                      <?php $is_first_sub_image = true;
+                            foreach($image_subgallery as $sub_image):
+                                $subgallery_attachments = wp_get_attachment_image_src( $sub_image->ID, 'full');
+                                echo '<a href="' . $subgallery_attachments[0] . '" class="colorbox colorbox-' . $subgallery_name . '" rel="' . $subgallery_name . '" title="' . $sub_image->post_excerpt . '">';
+                                if ($is_first_sub_image) {
+                                    $gallery_attachments = wp_get_attachment_image_src( $image->ID, 'thumbnail');
+                                    echo '<img src="' . $gallery_attachments[0] . '"></img>';
+                                }
+                                $is_first_sub_image = false;
+                                echo '</a>';
                             endforeach; ?>
-                    </li>
-      <?php     endforeach; // END $subgallery_posts
+                        </li>
+      <?php         endforeach; // END $subgallery_posts
+                }
             endforeach; // END $images
       ?>
       </ul>
