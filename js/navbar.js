985d1e77e929ec037858da2dbd611b5d379becfd
Kuba Odias <agrapho.pl@gmail.com>
2014-05-26 18:34:14 +0200

    Main menu refactoring.

--- js/navbar.js
+++ js/navbar.js
@@ -1,14 +1,15 @@
 +function($) {
 
   $(document).ready(function() {
-    var $navbar = $('.navbar-nav');
+    var $navbar = $('.navbar-nav:last-child');
     var offset = ($(window).width() - ($navbar.offset().left)) * 1.35;
     $('.navbar > .row-fluid').css('background-size', offset + 'px 100%');
   });
 
   $(".anchor-link").click(function() {
     // remove 'active' class from all links
-    $(".anchor-link").removeClass("active");
+    $(".home .main-menu-nav .anchor-link").removeClass("active");
+    $(".submenu-nav .anchor-link").removeClass("active");
     // add 'active' class to clicked link and scroll
     $(this).addClass("active");
     $('html, body').animate({
--- menu.php
+++ menu.php
@@ -1,22 +1,34 @@
 <?php $temp_query = $wp_query; ?>
-<?php if (!is_front_page()) { ?>
-         <div class="page-title-anchor anchor-link">
-           <a href="#"><?php echo $post->post_title; ?></a>
-         </div>
-<?php } ?>
-<ul class="nav navbar-nav">
-  <?php if (is_front_page()) { ?>
-    <li class="anchor-link"><a href="#o-nas">O nas</a></li>
-    <li class="anchor-link"><a href="#wnetrza">Wnętrza</a></li>
-    <li class="anchor-link"><a href="#grafika">Grafika</a></li>
-    <li class="anchor-link"><a href="#web-design">Web design</a></li>
-  <?php } else { ?>
-    <li class="anchor-link"><a href="<?php echo home_url('/'); ?>">Start</a></li>
-    <?php query_posts(array('post_parent' => get_the_ID(), 'post_type' => 'page', 'orderby' => 'menu_order', 'order' => 'asc')); while (have_posts()) { the_post(); ?>
-    <li class="anchor-link"><a href="#<?php echo $post->post_name; ?>"><?php the_title(); ?></a></li>
-  <?php } } ?>
-
+<?php 
+function append_main_menu_link($post, $slug, $name) {    
+    if (is_front_page())
+        $link_prefix = '#';
+    else
+	$link_prefix = '/';
+    if ($post->post_name == $slug)
+        $active_class = 'active';
+    else
+	$active_class = '';
+    echo '<li class="anchor-link ' . $active_class . '"><a href="' . $link_prefix . $slug . '">' . $name . '</a></li>';
+}
+?>
+<ul class="nav navbar-nav main-menu-nav">
+    <?php if (!is_front_page()) echo '<li class="anchor-link"><a href="/">Strona główna</a></li>'; ?>
+    <li class="anchor-link"><a href="<?php if (!is_front_page()) echo '/#'; else echo '#'; ?>o-nas">O nas</a></li>
+    <?php append_main_menu_link($post, 'wnetrza', 'Wnętrza');
+          append_main_menu_link($post, 'grafika', 'Grafika');
+          append_main_menu_link($post, 'web-design', 'Web design'); ?>
+    <?php if (is_front_page()) { ?>
     <li class="anchor-link"><a href="#kontakt">Kontakt</a></li>
+    <?php } ?>
 </ul>
+<?php if (!is_front_page()) { ?>
+    <ul class="nav navbar-nav submenu-nav">
+        <?php query_posts(array('post_parent' => get_the_ID(), 'post_type' => 'page', 'orderby' => 'menu_order', 'order' => 'asc')); while (have_posts()) { the_post(); ?>
+        <li class="anchor-link"><a href="#<?php echo $post->post_name; ?>"><?php the_title(); ?></a></li>
+        <?php } ?>
+        <li class="anchor-link"><a href="#kontakt">Kontakt</a></li>
+    </ul>
+<?php } ?>
 
 <?php $wp_query = $temp_query; ?>
