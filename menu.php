d6e81061b32006beb02b0c0ff1286a475c92e2a9
Kuba Odias <agrapho.pl@gmail.com>
2014-05-27 20:03:30 +0200

    Navbar dropdown menu.

--- menu.php
+++ menu.php
@@ -8,10 +8,18 @@ function append_main_menu_link($post, $slug, $name) {
     if ($post->post_name == $slug)
         $active_class = 'active';
     else
-	$active_class = '';
-    echo '<li class="anchor-link ' . $active_class . '"><a href="' . $link_prefix . $slug . '">' . $name . '</a></li>';
-}
-?>
+	$active_class = ''; ?>
+    <li class="dropdown anchor-link <?php echo $active_class; ?>"><a href="<?php echo $link_prefix . $slug; ?>"><?php echo $name; ?></a>
+        <ul class="dropdown-menu">
+        <?php $posts = get_posts(array('name' => $slug, 'posts_per_page' => 1, 'post_type' => 'page'));
+              query_posts(array('post_parent' => $posts[0]->ID, 'post_type' => 'page', 'orderby' => 'menu_order', 'order' => 'asc'));
+              while (have_posts()) {
+                  the_post(); ?>
+                  <li><a href="<?php echo $slug; ?>/#<?php echo $post->post_name; ?>"><?php the_title(); ?></a></li>
+        <?php } ?>
+        </ul>
+    </li>
+<?php }; ?>
 <ul class="nav navbar-nav main-menu-nav">
     <?php if (!is_front_page()) echo '<li class="anchor-link"><a href="/">Strona główna</a></li>'; ?>
     <li class="anchor-link"><a href="<?php if (!is_front_page()) echo '/#'; else echo '#'; ?>o-nas">O nas</a></li>
